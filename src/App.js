55c44b6aad38436eab8c5cd101b5ba12d26301c8
YFLooi <yihfoo@gmail.com>
2021-09-19 16:25:52 +0800

    Demo of react bootstrap accordions with rotating arrows

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import AccordionDisplay from "./Accordion";
+import { Container, Row } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container className="my-3">
+        <Row>
+          <h2>React bootstrap accordions with a rotating arrow indicator</h2>
+        </Row>
+      </Container>
+      <Container className="my-3">
+        <Row>
+          <AccordionDisplay
+            title="Here's a first title"
+            arrowPosition="left"
+            forceDefaultHide={true}
+          >
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam
+            </div>
+          </AccordionDisplay>
+        </Row>
+      </Container>
+      <Container className="my-3">
+        <Row>
+          <AccordionDisplay
+            title="Here's a second title"
+            arrowPosition="center"
+            forceDefaultHide={false}
+          >
+            <div>
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur
+            </div>
+          </AccordionDisplay>
+        </Row>
+      </Container>
+      <Container className="my-3">
+        <Row>
+          <AccordionDisplay
+            title="Here's a 3rd title"
+            arrowPosition="right"
+            forceDefaultHide={true}
+          >
+            <div>
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum
+            </div>
+          </AccordionDisplay>
+        </Row>
+      </Container>
+    </>
   );
 }
 
